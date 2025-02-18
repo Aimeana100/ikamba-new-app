562c94e0f53857b1bbb91664289591a7671abdec
Anathole <anathole.karinganire@amalitech.com>
2025-02-18 23:18:14 +0200

    commit lang/rw

---
+++ lang/rw/carbon.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'yesterday' => 'Ejo hashize saa :time',
+    'today' => 'Uyu munsi saa :time',
+    'tomorrow' => 'Ejo at :time',
+    'lastMonday' => 'Ku wa mbere ushize at :time',
+    'lastTuesday' => 'Ku wa kabiri ushize at :time',
+    'lastWednesday' => 'Ku wa gatatu ushize at :time',
+    'lastThursday' => 'Ku wa kane ushize at :time',
+    'lastFriday' => 'Ku wa gatanu ushize at :time',
+    'lastSaturday' => 'Ku wa gatandatu ushize at :time',
+    'lastSunday' => 'Ku cyumweru gishize at :time',
+    'thisMonday' => 'Uyu wa mbere at :time',
+    'thisTuesday' => 'Uyu wa kabiri at :time',
+    'thisWednesday' => 'Uyu wa gatatu at :time',
+    'thisThursday' => 'Uyu wa kane at :time',
+    'thisFriday' => 'Uyu wa gatanu at :time',
+    'thisSaturday' => 'Uyu wa gatandatu at :time',
+    'thisSunday' => 'Uyu cyumweru at :time',
+    'format' => 'l, F j, Y',
+];
